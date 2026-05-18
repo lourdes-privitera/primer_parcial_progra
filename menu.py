fbfc453e15a73f9ac76c6ea4bef3e1742d8f1614
lourdes-privitera <lulitaprivi@gmail.com>
2026-05-18 18:25:41 -0300

    Creo el menu de opciones

---
+++ menu.py
@@ -0,0 +1,16 @@
+def mostrar_menu():
+    print("")  
+    print("--SISTEMA DE PROCESAMIENTO DE CONTRASEÑAS--")    
+    print("1.   Ingresar contraseña")
+    print("2.   Validar nivel de seguridad")
+    print("3.   Contar tipos de caracteres")
+    print("4.   Buscar carácter específico")
+    print("5.   Mostrar contraseña invertida")
+    print("6.   Generar reporte estadístico")
+    print("7.   Verificar si es palindromo") 
+    print("8.   Ordenar caracteres de la contraseña")
+    print("9.   Salir")
+    print("")
+
+
+#print(mostrar_menu())
