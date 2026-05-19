7f7e348ddcdc98f59b0ccf1bf3758bc43a8433bb
lourdes-privitera <lulitaprivi@gmail.com>
2026-05-19 19:24:48 -0300

    Agrego las opciones 5,6,7 y 8 al menu

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ while bandera_programa: #DIJO NO USAR WHILE TRUE (USAR BANDERA¿?)
         contrasena = ingresar_contrasena()
         print("---Contraseña ingresada con exito!---")
         bandera_contraseña = True
-#-------------------------------------------------------FALTA PUSHEAR ESTO
-    elif opcion == 2 and bandera_contraseña == True: #NO PUSHEE ESTO
+    elif opcion == 2 and bandera_contraseña == True: 
         nivel_seguridad = validar_nivel_seguridad(contrasena)
         print(f"Nivel de seguridad: {nivel_seguridad}")
 
@@ -30,7 +29,30 @@ while bandera_programa: #DIJO NO USAR WHILE TRUE (USAR BANDERA¿?)
     elif opcion == 4 and bandera_contraseña == True:
         caracter = pedir_caracter()
         buscar_caracter(contrasena, caracter)
-#-------------------------------------------------------
+
+    elif opcion == 5 and bandera_contraseña == True:
+        contrasena_invertida = invertir_cadena(contrasena)
+        print(f"Contraseña original: {contrasena}")
+        print(f"Contraseña invertida: {contrasena_invertida}")
+
+    elif opcion == 6 and bandera_contraseña == True:
+        reporte_estadistico(contrasena) 
+
+    elif opcion == 7 and bandera_contraseña == True: 
+        verificacion = verificar_palindromo(contrasena)
+        if verificacion == True:
+            print("La contraseña es palíndromo")
+        else:
+            print("La contraseña NO es palíndromo")
+
+    elif opcion == 8 and bandera_contraseña == True:
+        orden = input("Ingrese tipo de orden (ascendente/descendente): ") #no llego a hacer una funcion que pida y valide el orden , la hago así para almenos presentar algo
+        while orden != "ascendente" and orden != "descendente":
+            orden = input("Reingrese ascendente o descendente: ")
+        contrasena_ordenada = ordenar_contrasena(contrasena, orden)
+        print(f"Contraseña original: {contrasena}")
+        print(f"Contraseña ordenada: {contrasena_ordenada}")
+        
     elif opcion == 9:
         print("SALIENDO...")
         bandera_programa = False
