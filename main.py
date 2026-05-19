5273b1066fd74e0c4a3d10cc0f0e39116ceb2964
lourdes-privitera <lulitaprivi@gmail.com>
2026-05-19 16:54:05 -0300

    Agrego las opciones 2, 3 y 4 al menu

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from validaciones import * #NO USAR EL ASTERISCO, LLAMAR DE A UNA ANTES DE ENTREGAR
 from menu import *
+from utilidades import *
+from analisis import *
 
 bandera_contraseña = False #cuando ingresen el 1 , se transforma ls bandera y puedo acceder a las opciones del menu 2 a 8  
 bandera_programa = True
@@ -17,7 +19,18 @@ while bandera_programa: #DIJO NO USAR WHILE TRUE (USAR BANDERA¿?)
         contrasena = ingresar_contrasena()
         print("---Contraseña ingresada con exito!---")
         bandera_contraseña = True
+#-------------------------------------------------------FALTA PUSHEAR ESTO
+    elif opcion == 2 and bandera_contraseña == True: #NO PUSHEE ESTO
+        nivel_seguridad = validar_nivel_seguridad(contrasena)
+        print(f"Nivel de seguridad: {nivel_seguridad}")
 
+    elif opcion == 3 and bandera_contraseña == True:
+        mostrar_cantidad_caracteres(contrasena)
+
+    elif opcion == 4 and bandera_contraseña == True:
+        caracter = pedir_caracter()
+        buscar_caracter(contrasena, caracter)
+#-------------------------------------------------------
     elif opcion == 9:
         print("SALIENDO...")
         bandera_programa = False
